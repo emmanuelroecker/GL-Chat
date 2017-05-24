c7a495d25c751e03ab1fde91bd4f239c497c36cf
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2017-05-24 20:29:48 +0000

    use nvm in systemd

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+service glchat stop
+rm /etc/systemd/system/glchat.service
 cp glchat.service /etc/systemd/system
+rm /etc/systemd/system/multi-user.target.wants/glchat.service
 cd /etc/systemd/system/multi-user.target.wants
 ln -s ../glchat.service .
+systemctl daemon-reload
+service glchat start
+systemctl status glchat
 
